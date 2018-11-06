81c5604bda83cf6507a6ef8f189373e4ee3c3243
jrayo00 <44790808+jrayo00@users.noreply.github.com>
2018-11-05 20:52:42 -0800

    Add files via upload

---
+++ gym_drop_tables.sql
@@ -0,0 +1,12 @@
+drop table FitnessMeasurement;
+drop table GymMember; 
+drop table Customer; 
+drop table Instructor;
+drop table Class;
+drop table TimePeriod;
+drop table ClassType;
+drop table Equipment;
+drop table Hosted;
+drop table Reservation;
+drop table IsQualifiledIn;
+commit;
---
+++ gym_insert_rows.sql
@@ -0,0 +1,32 @@
+--Data for table FitnessMeasurement
+
+
+--Data for table  GymMember
+
+
+--Data for table  Customer
+
+
+--Data for tableInstructor
+
+
+--Data for table Class
+
+
+--Data for tableTimePeriod
+
+
+--Data for table ClassType
+
+
+--Data for tableEquipment
+
+
+--Data for tableHosted
+
+
+--Data for tableReservation
+
+
+--Data for tableIsQualifiledIn
+  
