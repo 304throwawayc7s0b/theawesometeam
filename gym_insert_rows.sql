9430e42d1e64b36be22d723cbb578c435e690c90
jrayo00 <44790808+jrayo00@users.noreply.github.com>
2018-11-14 18:13:34 -0800

    Update gym_insert_rows.sql

--- gym_insert_rows.sql
+++ gym_insert_rows.sql
@@ -1,5 +1,55 @@
 --Data for table  Customer
-insert into customer values (1,'778-319-3333','Sarina','15168 32nd Street', '1111-111-1111');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (1, '799-275-7846', 'Dynah Peachment', '45257 Welch Place', '6709681032674994');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (2, '381-558-4510', 'Garland Mollatt', '972 Summerview Lane', '4041372099032506');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (3, '896-774-6994', 'Guss Deniso', '749 Heath Road', '4913449074547172');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (4, '371-646-9456', 'Bibbie Campanelli', '0 Meadow Ridge Point', '3585917145967529');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (5, '160-178-5336', 'Jourdan Vitte', '417 Veith Lane', '6706781493382294');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (6, '118-548-9550', 'Layla Fleis', '02140 Oak Way', '30183411472812');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (7, '706-259-5408', 'Nevile Heaseman', '08 Columbus Center', '4982790752968321');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (8, '890-904-1192', 'Damien Klinck', '4343 Summit Pass', '5100178829494527');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (9, '617-914-6885', 'Orsa Hefford', '8 Brown Avenue', '633110175053863218');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (10, '994-495-6987', 'Warde Verriour', '0336 Summit Way', '5602241066758401');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (11, '218-569-6028', 'Moira Grendon', '4 Alpine Circle', '201918670990837');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (12, '509-384-3192', 'Aurie Chew', '3 Starling Circle', '201576878686332');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (13, '845-290-4022', 'Ellie Lomasney', '758 Oneill Avenue', '633110991491954314');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (14, '529-202-3400', 'Traci Loney', '52 Anthes Street', '3589300839948558');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (15, '746-985-8223', 'Alastair Cains', '01 Kim Terrace', '4903387247367411');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (16, '296-689-6167', 'Philippa Forsaith', '7 Lerdahl Circle', '3586515737103898');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (17, '898-656-0902', 'Gun Revett', '454 Manufacturers Hill', '30433269181266');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (18, '933-880-5831', 'Cobb Biggs', '68795 Loeprich Hill', '3584069496025985');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (19, '433-133-4552', 'Georgeanna Jollie', '7 Mandrake Trail', '3577309719879621');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (20, '359-518-8161', 'Rose Trewin', '64 Sycamore Parkway', '3545154918273484');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (21, '698-134-6809', 'Johannes Stell', '86 Kensington Park', '3575906810408929');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (22, '166-270-9824', 'Bryana Habben', '6 Delaware Center', '3566693977458696');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (23, '998-301-0772', 'Noel Orrum', '633 Gina Pass', '3552754380995033');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (24, '115-495-0419', 'Greggory Falconer', '400 Muir Point', '3565720448793327');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (25, '356-231-6566', 'Genvieve Oakenfall', '36 Sunnyside Avenue', '3581451655597744');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (26, '621-448-9783', 'Trueman Bickersteth', '3 Judy Avenue', '4017954226932649');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (27, '166-800-7470', 'Emlynn Van Cassel', '55125 Talisman Court', '5602245990405641');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (28, '522-317-4350', 'Sterling Reuven', '20 Alpine Trail', '56022205242982081');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (29, '697-241-2868', 'Mella Maskell', '8680 Hooker Point', '5048370231445479');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (30, '126-316-5745', 'Othelia Rubinfajn', '3473 Bashford Parkway', '3560903076978379');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (31, '771-627-2674', 'Harlene Yakunikov', '4646 Roxbury Park', '3535356812997009');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (32, '398-640-3862', 'Almire Thirtle', '12 Parkside Hill', '3548762764202873');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (33, '749-980-2222', 'Dorry Couvert', '4740 Shopko Drive', '5318549519692232');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (34, '244-402-6174', 'Blane Van Driel', '88704 Fairview Park', '4405566140459424');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (35, '522-919-2114', 'Lucian Dalyiel', '76496 School Drive', '5048373237442003');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (36, '694-619-2787', 'Michaeline Killiner', '07 Eastwood Parkway', '6767767792427423');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (37, '110-377-7362', 'Anselma Antalffy', '5084 Gulseth Center', '6333422409323475');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (38, '672-553-2218', 'Katherine Arthurs', '36 Del Mar Alley', '3573226256622517');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (39, '511-539-5818', 'Arleen Franckton', '661 Norway Maple Hill', '3530814167429312');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (40, '307-449-0377', 'Bernhard Goricke', '79767 Logan Plaza', '3555274218224941');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (41, '650-742-0195', 'Sascha Noriega', '23321 School Plaza', '3544623827832441');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (42, '685-648-1140', 'Fabe Marcussen', '55 Victoria Parkway', '3539093868409896');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (43, '914-870-4395', 'Poppy Stallion', '9 Dahle Parkway', '5602257785234433');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (44, '668-530-9635', 'Ludovico Gylle', '691 Melvin Terrace', '3532227736790882');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (45, '542-151-9543', 'Bevin Riddett', '7899 Hanson Court', '0604159649111666964');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (46, '866-440-8489', 'Kinna Blowes', '33 Kennedy Place', '3564622380322597');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (47, '660-801-3771', 'Archaimbaud Wile', '8 Elka Street', '3565076989388377');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (48, '265-901-9304', 'Barnett Garrals', '8 Bowman Lane', '3572786291136857');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (49, '891-325-5326', 'Kessiah Yakovitch', '0645 Huxley Street', '6709636511014203');
+insert into Customer (CustomerID, Phone, Name, Address, CreditCard) values (50, '779-175-3613', 'Giustino Billings', '08865 Bobwhite Point', '3543314153536555');
+
 
 
 
