6e48c9f9a26aaa58bf220fa9e5380f05d39d5511
derek <derekha19@gmail.com>
2018-11-17 17:37:29 -0800

    headerupdate

--- web/customer_fitness.php
+++ web/customer_fitness.php
@@ -11,9 +11,9 @@
 
 <ul class="header">
     <li class="dropdown header">
-        <a href="member_mng.php">Membership Management</a>
+        <a href="member_add.php">Membership Management</a>
         <div class="dropdown_list">
-            <a href="member_delete.php">View Members</a>
+            <a href="member_add.php">Add Membership</a>
             <a href="measurements_add.php">Add Member Measurements</a>
         </div>
     </li>
